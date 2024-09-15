5a63bb68800ae1f780dba4e632e162a5333f6265
Tatiparthi Madhusudhan Rao <madhusudhan.t@fpt.com>
2024-09-15 18:52:25 +0530

    terraform varibles commited"

---
+++ DevApp/Terraform_varables/Security_Groups.tf
@@ -0,0 +1,54 @@
+resource "aws_security_group" "vpc_ssh"{ 
+    name = "vpc_ssh"
+    description = "Dev VPC SSH"
+    
+    ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
+
+resource "aws_security_group" "vpc_web"{ 
+    
+    ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
+
